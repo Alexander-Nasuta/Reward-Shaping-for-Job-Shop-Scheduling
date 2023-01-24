6492deb0251567e4eeea2f496c72a0b6c3e0e560
Alexander Nasuta <alexander.nasuta@rwth-aachen.de>
2023-01-24 11:28:42 +0100

    10x10 scripts (orb04)

---
+++ src/jsp_experiments/graph_jsp/10x10/orb04_samsonov_no_ls_graph_jsp_env_tuning_wrapper.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+for i in {1..50}; do python orb04_samsonov_no_ls_graph_jsp_env_tuning.py; done
---
+++ src/jsp_experiments/graph_jsp/10x10/run_sweeps_with_screen_sessions.sh
@@ -0,0 +1,14 @@
+
+screen -S nasuta_ls -p 0 -X bash -c "bash /Users/qwerty/PycharmProjects/jsp-experiments/src/jsp_experiments/graph_jsp/10x10/orb04_nasuta_ls_graph_jsp_env_tuning_wrapper.sh"
+
+screen -S nasuta_no_ls -p 0 -X bash -c "bash /Users/qwerty/PycharmProjects/jsp-experiments/src/jsp_experiments/graph_jsp/10x10/orb04_nasuta_no_ls_graph_jsp_env_tuning_wrapper.sh"
+
+screen -S zhang_ls -p 0 -X bash -c "bash /Users/qwerty/PycharmProjects/jsp-experiments/src/jsp_experiments/graph_jsp/10x10/orb04_zhang_ls_graph_jsp_env_tuning_wrapper.sh"
+
+screen -S zhang_no_ls -p 0 -X bash -c "bash /Users/qwerty/PycharmProjects/jsp-experiments/src/jsp_experiments/graph_jsp/10x10/orb04_zhang_no_ls_graph_jsp_env_tuning_wrapper.sh"
+
+screen -S sam_ls -p 0 -X bash -c "bash /Users/qwerty/PycharmProjects/jsp-experiments/src/jsp_experiments/graph_jsp/10x10/orb04_samsonov_ls_graph_jsp_env_tuning_wrapper.sh"
+
+screen -S sam_no_lstassel_ls -p 0 -X bash -c "bash /Users/qwerty/PycharmProjects/jsp-experiments/src/jsp_experiments/graph_jsp/10x10/orb04_samsonov_no_ls_graph_jsp_env_tuning_wrapper.sh"
+
+screen -S tassel_no_ls -p 0 -X bash -c "bash /Users/qwerty/PycharmProjects/jsp-experiments/src/jsp_experiments/graph_jsp/10x10/orb04_graph-tassel_ls_graph_jsp_env_tuning_wrapper.sh"
