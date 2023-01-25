b0e704cd46a1d6a878cbb9b3133a1bd7efa47f1a
Alexander Nasuta <alexander.nasuta@rwth-aachen.de>
2023-01-25 10:20:53 +0100

    10x10 orb4

---
+++ src/jsp_experiments/graph_jsp/10x10/orb04_samsonov_no_ls_graph_jsp_env_tuning.py
@@ -0,0 +1,20 @@
+
+import gym
+import wandb as wb
+
+from jsp_experiments.graph_jsp.perform_sweep_run import perform_run
+
+gym.envs.register(
+    id='gjsp-v0',
+    entry_point='graph_jsp_env.disjunctive_graph_jsp_env:DisjunctiveGraphJspEnv',
+    kwargs={},
+)
+
+if __name__ == '__main__':
+    sweep_id = 'bpjyry4k'
+    wb.agent(
+        sweep_id,
+        function=perform_run,
+        count=1,
+        project="reward-functions-comparison"
+    )
